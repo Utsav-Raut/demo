d8ff9c4a237c1a5a4ecd7d1b4ee31a770418ba46
Utsav <utsav.raut@yahoo.com>
2019-03-26 11:19:22 +0530

    Create row_count_validation.py

---
+++ utils/row_count_validation.py
@@ -0,0 +1,16 @@
+from utils import hive_table_operations, file_operations
+
+def get_row_count_diff(spark, df, field_name, table_name, file_path):
+    hive_tbl_row_count = hive_table_operations.get_hive_tbl_row_count(spark, df, field_name, table_name)
+    input_file_row_count = file_operations.get_file_row_count(file_path, field_name)
+
+    row_count_diff = hive_tbl_row_count - input_file_row_count
+
+    if row_count_diff == 0:
+        output = 'Hive Table and files have equal number of Transactions IDs'
+    elif row_count_diff > 0:
+        output = 'Hive Table has more Transactions IDs than the files'
+    else:
+        output = 'Files have more Transactions IDs than Hive table'
+
+    return row_count_diff, output, hive_tbl_row_count, input_file_row_count
